6f95587239a4e0a2e39e139880a97f87cdc60f48
Macbook Pro <macbookpro@Macbooks-MacBook-Pro-3.local>
2026-02-24 14:11:23 -0500

    Initial commit

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css"
+
+function Favorite (){
+    return <div className="favorites-empty">
+        <h2> No favorite Movies Yet</h2>
+        <p>Starting adding movies to your favorites</p>
+    </div>
+}
+
+export default Favorite
---
+++ frontend/src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "361c8a6da869a3d51c3a661a5a93766f"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results 
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results 
+}
+
+
