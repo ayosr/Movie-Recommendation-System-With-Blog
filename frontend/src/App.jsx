80a0454ae78d5a70c18a7c643e4942ec9adb1b57
alexislsimmerman-lang <alexis.l.simmerman@gmail.com>
2026-05-06 15:17:35 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,24 @@ import Favorites from "./pages/Favorites"
 import Home from "./pages/Home"
 import {Routes, Route} from "react-router-dom"
 import NavBar from "./components/NavBar"
+import BlogList from "./pages/BlogList";
+import Post from "./components/Post";
+import CreateBlog from "./pages/CreateBlog";
+import { initialBlogData } from "./data/blogData";
 
 function App() {
+  const [blogs, setBlogs] = useState(() => {
+    const saved = localStorage.getItem("movie-blog-data");
+    return saved ? JSON.parse(saved) : initialBlogData;
+    useEffect(() => {
+    localStorage.setItem("movie-blog-data", JSON.stringify(blogs));
+  }, [blogs]);
+    const addBlog = (newPost) => {
+    setBlogs([newPost, ...blogs]);
+  };
+const deleteBlog = (id) => {
+    setBlogs(blogs.filter((blog) => blog.blogID !== id));
+  };
   return (
     <div>
       <NavBar />
@@ -14,6 +30,11 @@ function App() {
         <Routes>
           <Route path="/" element={ <Home />}/>
           <Route path="/favorites" element={ <Favorites/>}/>
+          <Route path="/blog" element={<BlogList blogs={blogs} />} />
+          <Route path="/blog/new" element={<CreateBlog addBlog={addBlog} />} />
+          <Route 
+            path="/blog/:id" 
+            element={<Post blogs={blogs} deleteBlog={deleteBlog} />}
         </Routes>
       </main>
     </div>
