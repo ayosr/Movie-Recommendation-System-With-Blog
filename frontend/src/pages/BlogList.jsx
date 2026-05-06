e02030dff030c132993ac3bbdf630157428904b1
alexislsimmerman-lang <alexis.l.simmerman@gmail.com>
2026-05-06 15:28:06 -0400

    Create BlogList.jsx

---
+++ frontend/src/pages/BlogList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import BlogCard from "../components/BlogCard";
+
+const BlogList = ({ blogs }) => (
+  <div className="blog-list-container">
+    <header className="blog-header">
+      <h1>Movie Insights</h1>
+      <Link to="/blog/new" className="btn-primary">+ Write a Review</Link>
+    </header>
+    <div className="blog-grid">
+      {blogs.map(blog => (
+        <BlogCard key={blog.blogID} blog={blog} />
+      ))}
+    </div>
+  </div>
+);
+
+export default BlogList;
