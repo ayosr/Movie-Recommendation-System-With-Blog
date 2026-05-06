e7c5bf0c5be8fde43c72c7d79d2abf062a044c7b
alexislsimmerman-lang <alexis.l.simmerman@gmail.com>
2026-05-06 15:34:56 -0400

    Update CreateBlog.jsx

--- frontend/src/pages/CreateBlog.jsx
+++ frontend/src/pages/CreateBlog.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../css/BlogCard.css"
 
 const CreateBlog = ({ addBlog }) => {
   const [title, setTitle] = useState("");
