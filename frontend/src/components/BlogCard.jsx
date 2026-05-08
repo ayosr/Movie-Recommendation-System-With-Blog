6ebdba45162bd095bb538eb86540d4e557e0f6b0
alexislsimmerman-lang <alexis.l.simmerman@gmail.com>
2026-05-08 11:29:57 -0400

    Update BlogCard.jsx

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,30 +1,32 @@
-import React, { Component } from "react";
-import { Card, CardText, CardBody, CardTitle, Button } from "reactstrap";
-import { withRouter } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-class BlogCard extends Component {
-  blogDetail = (event) => {
-    this.props.history.push(`/blog/${event.target.id}`);
-  };
-  render() {
-    const blog = this.props;
-    return (
-      <>
-        <Card>
-          <CardBody>
-            <CardTitle tag="h5">{blog.title}</CardTitle>
-          </CardBody>
-          <img width="100%" src={blog.imageUrl} alt={blog.title} />
-          <CardBody> 
-            <CardText>{blog.content.substr(0, 100)}...</CardText>
-            <Button onClick={() => this.props.history.push(`/blog/${blog.blogID}`)}>
-              Read More
-            </Button>
-          </CardBody>
-        </Card>
-      </>
-    );
-  }
-}
+const BlogCard = ({ blog }) => {
+  const navigate = useNavigate();
 
-export default withRouter(BlogCard);
+  return (
+    <div className="blog-card" onClick={() => navigate(`/blog/${blog.blogID}`)}>
+      {blog.imageUrl && (
+        <img className="blog-card-img" src={blog.imageUrl} alt={blog.title} />
+      )}
+      <div className="blog-card-body">
+        <h3 className="blog-card-title">{blog.title}</h3>
+        <p className="blog-card-meta">By {blog.author} · {blog.date}</p>
+        <p className="blog-card-excerpt">
+          {blog.content ? `${blog.content.substr(0, 100)}...` : ""}
+        </p>
+        <button
+          className="btn-primary"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate(`/blog/${blog.blogID}`);
+          }}
+        >
+          Read More
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
