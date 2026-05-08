f03fb5ae12f335299118ba0a196dd2e4dd3316e2
alexislsimmerman-lang <alexis.l.simmerman@gmail.com>
2026-05-08 11:23:18 -0400

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,6 +8,7 @@ function NavBar(){
         </div>
         <div className="navbar-links">
             <Link to="/" className ="nav-link">Home</Link>
+            <Link to="/blog" className ="nav-link">Blog</Link>
             <Link to="/favorites" className ="nav-link">Favorites</Link>
         </div>
     </nav>
